1e32f78c992fecc823a97566e9d4edaa3544ae21
e <1127431465@qq.com>
2017-11-23 16:57:53 +0800

    globalData数据请求

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 App({
   onLaunch (options) {
+    // 请求数据
+    wx.request({
+      url: 'https://resources.ninghao.net/wxapp-case/db.json',
+      success:(response)=>{
+        console.log(response);
+        Object.assign( this.globalData,response.data);
+        console.log(this.globalData)
+      }
+    })
   },
   onShow (options) {
   },
@@ -8,5 +17,10 @@ App({
   onError (error) {
   },
   globalData: {
+    slides:[{
+      id:'123'
+    },{
+      id:'345'
+    }]
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -2,7 +2,12 @@ const app = getApp()
 
 Page({
   data: {
+    slides: null,
   },
   onLoad () {
+    // 全局数据 globalData
+    this.setData({
+      slides:app.globalData.slides
+    });
   }
 })
